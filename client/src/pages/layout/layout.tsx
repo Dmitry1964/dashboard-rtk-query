5305c10b4a9f8011f60397ebd8c9c66b98410ced
Dmitry64 <nosdm64@gmail.com>
2025-11-09 11:33:27 +0300

    Добавляет моковые данные

--- client/src/pages/layout/layout.tsx
+++ client/src/pages/layout/layout.tsx
@@ -1,26 +1,26 @@
 import cls from './layout.module.scss';
-import { Navbar } from 'src/widgest/navbar';
+// import { Navbar } from 'src/widgest/navbar';
 import { Outlet} from 'react-router-dom';
-import { Header } from 'src/widgest/header';
-import { RootState } from 'src/store/store';
-import { useSelector } from 'react-redux';
-import { getHeaderTitle } from 'src/shared/service';
+// import { Header } from 'src/widgest/header';
+// import { RootState } from 'src/store/store';
+// import { useSelector } from 'react-redux';
+// import { getHeaderTitle } from 'src/shared/service';
 // import { ModalPage } from 'src/enteties/modal-page';
 
 const Layout = () => {
 
-  const  path = useSelector((state : RootState) => state.location.pathName);
+  // const  path = useSelector((state : RootState) => state.location.pathName);
 
-  const title =  getHeaderTitle(path)
+  // const title =  getHeaderTitle(path)
   
 
   return (
     <div className={cls.layout}>
-      <Navbar />
+      {/* <Navbar /> */}
       <main className={cls.layout__main}>
         <section className={cls.layout__wrapper}>
           {/* <ModalPage description="Вы успешно зарегистрировались" /> */}
-          <Header title={title} />
+          {/* <Header title={title} /> */}
           <Outlet />
         </section>
       </main>
--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,16 +1,16 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import locationReducer from 'src/slicies/location-slice/location-slice';
-import userRegisterReducer from 'src/slicies/user-auth-slice/user-register-slice';
-import newPartnerReducer from 'src/slicies/new-partners-slice/new-partners-slice';
-import partnersListReducer from 'src/slicies/partners-list-slice/partners-list-slice';
-import partnerUpdateReducer from 'src/slicies/partner-update-slice/partner-update-slice';
+// import locationReducer from 'src/slicies/location-slice/location-slice';
+// import userRegisterReducer from 'src/slicies/user-auth-slice/user-register-slice';
+// import newPartnerReducer from 'src/slicies/new-partners-slice/new-partners-slice';
+// import partnersListReducer from 'src/slicies/partners-list-slice/partners-list-slice';
+// import partnerUpdateReducer from 'src/slicies/partner-update-slice/partner-update-slice';
 
 const rootReducer = combineReducers({
-  location: locationReducer,
-  userRegister: userRegisterReducer,
-  newPartner: newPartnerReducer,
-  partnersList: partnersListReducer,
-  partnerUpdate: partnerUpdateReducer,  
+  // location: locationReducer,
+  // userRegister: userRegisterReducer,
+  // newPartner: newPartnerReducer,
+  // partnersList: partnersListReducer,
+  // partnerUpdate: partnerUpdateReducer,  
   // userStatus: userStatusReducer,
   });
 
